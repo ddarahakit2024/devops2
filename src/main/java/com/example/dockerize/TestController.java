6df4c936e9550528780622c4e78d641a680ff1c4
ddarahakit <ddarahakit@gmail.com>
2024-08-19 15:05:58 +0900

    test4

--- src/main/java/com/example/dockerize/TestController.java
+++ src/main/java/com/example/dockerize/TestController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @RequestMapping(method = RequestMethod.GET, value = "/test")
     public String test() {
-        return "test7";
+        return "test8";
     }
 }
